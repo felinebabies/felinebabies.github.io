9fa8d80cce2683b1ecf8f8a6da50439b66f52a37
Syuji <commentonly.is@gmail.com>
2017-11-09 00:16:03 +0900

    live reloadの接続先をlocalhostに変更した。

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@
 
 # Reload the browser automatically whenever files change
 configure :development do
-  activate :livereload, :host => `hostname`.chomp
+  activate :livereload, :host => 'localhost'
 end
 
 # Methods defined in the helpers block are available in templates
